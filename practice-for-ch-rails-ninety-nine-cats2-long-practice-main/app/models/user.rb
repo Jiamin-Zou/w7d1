5a0bc1e334b65a7905222a0bb6231a0f374902bd
Jiamin-Zou <jiaminz622@gmail.com>
2023-04-17 14:46:19 -0400

    Added User model

---
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
@@ -0,0 +1,44 @@
+class User < ApplicationRecord
+    validates :username, :password_digest, :session_token, presence: true
+    validates :username, :session_token, uniqueness: true
+    validates :password, length: { minimum: 6, allow_nil: true }
+    after_initialize :ensure_session_token
+    attr_reader :password
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def is_password?(password)
+        Bcrpyt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+
+    def generate_unique_session_token
+        loop do
+            session_token = SecureRandom::urlsafe_base64
+            return session_token unless User.exist?(session_token: session_token)
+        end
+    end
+
+    def reset_session_token!
+        self.session_token = generate_unique_session_token
+        self.save!
+    end
+
+    private
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+    end
+end
